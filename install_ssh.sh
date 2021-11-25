afc98f4b30843f4b57dd0d1f601209a5445fd22f
0xOtoguro <93055914+0xOtoguro@users.noreply.github.com>
2021-11-25 12:54:22 +0100

    github auth token

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,20 +1,5 @@
 #! /bin/bash
 
-mkdir -p ~/.ssh
-
-echo "Listing"
-ls ~/.ssh
-echo "Listing done"
-
-echo "$SSH_KEY_PUB" > ~/.ssh/id_rsa.pub
-echo "$SSH_KEY" > ~/.ssh/id_rsa
-cp ./config ~/.ssh/config
-chmod 600 ~/.ssh/id_rsa*
-
-echo "Listing"
-ls -la ~/.ssh
-echo "Listing done"
-
-ssh-add ~/.ssh/id_rsa
+sed "s/<github_personal_access_token>/${GHTOKEN}/g" package.ci.json > package.json
 
 yarn vercel
